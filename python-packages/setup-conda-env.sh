bae1db8ce207f8406bd4249cc83d87029316a668
Minesh A. Jethva <minesh.1291@gmail.com>
2020-05-29 10:28:49 +0300

    Create setup-conda-env.sh

---
+++ python-packages/setup-conda-env.sh
@@ -0,0 +1,6 @@
+MYENV="ml4ts-env"
+
+# To create an environment with specified python version and required libraries:
+conda create -n $MYENV python=3.7 --file=requirements.txt.md
+
+# ref. https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
